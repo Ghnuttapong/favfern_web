cda72cc57e3902a78358ad025dc7166e276e71a9
yotsarun <yotsarunjjrp@gmail.com>
2023-09-10 18:22:51 +0700

    update

--- js/custom.js
+++ js/custom.js
@@ -7,46 +7,9 @@ myApp.controller("myController", function ($scope, $http, $timeout) {
   const year = today.getFullYear();
 
   let fn_jquery = function () {
-    wow = new WOW();
-    wow.init();
     $(document).ready(function (e) {
       $(".slider").bxSlider({
         pager: false,
-      });
-    });
-
-    $(window).on("scroll", function () {
-      var bodyScroll = $(window).scrollTop(),
-        navbar = $(".navbar");
-
-      if (bodyScroll > 50) {
-        $(".navbar-logo img").attr("src", "./images/logo-black.png");
-        navbar.addClass("nav-scroll");
-      } else {
-        $(".navbar-logo img").attr("src", "./images/logo.png");
-        navbar.removeClass("nav-scroll");
-      }
-      $scope.$apply();
-    });
-    $(window).on("load", function () {
-      var bodyScroll = $(window).scrollTop(),
-        navbar = $(".navbar");
-
-      if (bodyScroll > 50) {
-        $(".navbar-logo img").attr("src", "./images/logo-black.png");
-        navbar.addClass("nav-scroll");
-      } else {
-        $(".navbar-logo img").attr("src", "./images/logo.png");
-        navbar.removeClass("nav-scroll");
-      }
-      $scope.$apply();
-
-      $.scrollIt({
-        easing: "swing", // the easing function for animation
-        scrollTime: 900, // how long (in ms) the animation takes
-        activeClass: "active", // class given to the active nav element
-        onPageChange: null, // function(pageIndex) that is called when page is changed
-        topOffset: -63,
       });
     });
   };
